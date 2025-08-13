16a049a0ec21bd8bc3b6041978508647edf5dfa3
chandan vishwakarma <chandanvishw11002@gmail.com>
2025-08-13 08:02:49 +0530

    changes has been made

--- app.js
+++ app.js
@@ -2,16 +2,11 @@ function add (a,b){
     return a + b
 }
 
-console.log(add(4,6))
-
 function sub(a, b){
     return a-b;
 }
-
-console.log(sub(6-4));
 
 
 function mul(a,b){
     return a*b
 }
-console.log(mul(2,4))
